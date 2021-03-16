4fcd845d6b2ab2c3bb457389a745d01763abafd8
Debjyoti Guha Roy Choudhury <debjyotiguharoychoudhury@Debjyotis-MacBook-Pro.local>
2021-03-17 01:14:34 +0530

    Performance decorator for future use

---
+++ performance_decorator.py
@@ -0,0 +1,19 @@
+from time import time
+
+def performance_monitor(fn):
+	
+	def wrapped_func(*args, **kwargs):
+		t1 = time()
+		result = fn()
+		t2 = time()
+		print(f'took {t2-t1} secs for the function \'{fn.__name__}\' to run')
+		return result
+	return wrapped_func
+
+@performance_monitor
+def random_func():
+	for i in range(0,100000):
+		print(i*5)
+
+random_func()
+
